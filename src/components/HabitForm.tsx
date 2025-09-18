b9e277685df637a807eeb928e9d29878a1c8532a
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-18 17:23:27 +1000

    [dyad] Fixed a typo in the zod import statement in . - wrote 1 file(s)

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import *s z from 'zod';
+import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import {
   Form,
