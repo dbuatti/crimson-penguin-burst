b4018be57e7adab7a611ea734bd80a2dfef41a85
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-18 13:05:56 +1000

    [dyad] Addressed React Router warnings and provided steps to debug Supabase 401 error. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}> {/* Added future flags */}
         <SessionContextProvider> {/* Wrap the entire app with SessionContextProvider */}
           <Routes>
             <Route path="/" element={<Index />} />
